c0c19721ed68ec855d564a82bfaa9924b62c7726
grace22411 <olayinkagrace112.ob@gmail.com>
2021-02-12 16:41:49 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+class Header extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <header class="container">
+      <div class="row">
+          <div class="logo col-md-3">
+              <img src="images/aiki-logo.png" alt="">
+          </div>
+          <div class="col-md-9">
+              <ul>
+                  <li> <a href="index.html"> Home</a></li>
+                  <li><a href="about.html"> About</a></li>
+                  <li><a href="service.html"> Services</a></li>
+                  <li><a href="contact.html"> Contact</a></li>
+                  <li><a href="RequestService.html"> Request Our Services</a></li>
+                  <li><a href="signup.html"> <button>Sign up for free</button></a>  
+                  </li>
+              </ul>
+          </div>
+      </div>
+
+  </header>
+      `;
+  }
+}
+
+class Footer extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = `
+      <footer>
+      <div class="container">
+          <div class="row">
+              <div class="col-md-4">
+                  <div class="logo">
+                      <img src="images/aiki-white logo.png" alt="Aiki Logo White">
+                  </div>
+              </div>
+              <div class="col-md-4">
+                  <h3>Company</h3>
+                  <li> <a href="index.html"> Home</a></li>
+                <li><a href="about.html"> About</a></li>
+                <li><a href="service.html"> Services</a></li>
+                <li><a href="contact.html"> Contact</a></li>
+                <li><a href="signup.html">Apply for Job</a> 
+              </div>
+              <div class="col-md-4">
+                  <h3>Contact</h3>
+                  <i class="fab fa-facebook-square"></i>
+                  <i class="fab fa-instagram-square"></i>
+                  <i class="fab fa-linkedin"></i>
+                  <i class="fab fa-twitter-square"></i>
+              </div>
+          </div>
+      </div>
+      <div class="container post-footer">
+          <div class="row">
+              <div class="col-md-6">
+                  <p>All rights Reserved,2021</p>
+              </div>
+              <div class="col-md-6 privacy">
+                  <p>Privacy Policy | Terms of Condition </p>
+              </div>
+          </div>
+      </div>
+  </footer>
+        `;
+    }
+  }
+customElements.define('main-header', Header);
+customElements.define('main-footer', Footer);
